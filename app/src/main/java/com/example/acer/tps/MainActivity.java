9306375f3f84872839a8b4de2f3284cd58ce91e8
solo <1163315115@qq.com>
2016-03-26 10:29:09 +0800

    提交鸟6

--- app/src/main/java/com/example/acer/tps/MainActivity.java
+++ app/src/main/java/com/example/acer/tps/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        System.out.print("safddsfds");
         setContentView(R.layout.activity_main);
     }
 }
